7d79cb7d5e6c67c30aca199e8fb871ff2206b5fa
KOSTANIKOLAKOPOULOS <kostanikolakopoulos@yahoo.gr>
2019-12-09 22:17:25 +0200

    first section

---
+++ EXCERCISE_1.py
@@ -0,0 +1,13 @@
+#INSPECTOR LEKAS
+lista=[]
+i=0
+while True:
+    i+=1
+    number=input("give the number or stop")
+    if number=="stop":
+        break
+    lista.append(number)
+    if i==5 :
+        break
+        
+print(lista)
