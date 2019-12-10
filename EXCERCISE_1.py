e0e287af1f347cf158b27bfd77f0b591f8e73f27
KOSTANIKOLAKOPOULOS <kostanikolakopoulos@yahoo.gr>
2019-12-10 09:05:50 +0200

    update

--- EXCERCISE_1.py
+++ EXCERCISE_1.py
@@ -1,6 +1,16 @@
 #INSPECTOR LEKAS
 lista=[]
 i=0
+lista=[1,1,1,1,1]
+def add():
+    k=0
+    sum=0
+    for j in lista:
+        sum=j+sum
+        k+=1
+    return sum/k
+print(add())
+'''        
 while True:
     i+=1
     number=input("give the number or stop")
@@ -9,5 +19,9 @@ while True:
     lista.append(number)
     if i==5 :
         break
-        
-print(lista)
+for i in lista:
+    if i<11:
+        lista1.append(i)
+len(lista1)
+    if i<add()/2:    
+'''
